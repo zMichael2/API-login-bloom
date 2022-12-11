d6a169b22309cfdb3c2ba6b41d58ea27d66eeb80
zMichael2 <maikoldesigner@gmail.com>
2022-12-10 21:51:10 -0500

    delete dist

--- dist/interface/authUser.interface.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Roles = void 0;
-var Roles;
-(function (Roles) {
-    Roles["admin"] = "admin";
-    Roles["user"] = "user";
-})(Roles = exports.Roles || (exports.Roles = {}));
-//# sourceMappingURL=authUser.interface.js.map
